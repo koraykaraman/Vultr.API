5f43a7f4f2c4dc67c7f4c9f236e92fbf00cffa06
Koray KARAMAN <koraykaraman@hotmail.com>
2018-08-04 16:40:08 +0300

    Added ISO Image operations.

---
+++ Clients/ISOImageClient.vb
@@ -0,0 +1,68 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+Imports Vultr.API.Models.Responses
+
+Namespace API.Clients
+    Public Class ISOImageClient
+        ReadOnly _ApiKey As String
+
+        Public Sub New(ByVal ApiKey As String)
+            _ApiKey = ApiKey
+        End Sub
+
+        ''' <summary>
+        ''' List all ISOs currently available on this account.
+        ''' </summary>
+        ''' <returns>List of all ISOs currently available on this account.</returns>
+        Function GetISOImages() As ISOImageResult
+            Dim answer As New Dictionary(Of String, ISOImage)
+            Dim httpResponse = Extensions.ApiClient.ApiExecute("iso/list", _ApiKey)
+            If httpResponse.StatusCode = 200 Then
+                Using streamReader = New StreamReader(httpResponse.GetResponseStream())
+                    Dim st = streamReader.ReadToEnd()
+                    answer = JsonConvert.DeserializeObject(Of Dictionary(Of String, ISOImage))(If(st = "[]", "{}", st))
+                End Using
+            End If
+            Return New ISOImageResult With {.ApiResponse = httpResponse, .ISOImages = answer}
+        End Function
+        ''' <summary>
+        ''' List public ISOs offered in the Vultr ISO library.
+        ''' </summary>
+        ''' <returns>List of all public ISOs offered in the Vultr ISO library.</returns>
+        Function GetPublicISOImages() As ISOImageResult
+            Dim answer As New Dictionary(Of String, ISOImage)
+            Dim httpResponse = Extensions.ApiClient.ApiExecute("iso/list_public", _ApiKey)
+            If httpResponse.StatusCode = 200 Then
+                Using streamReader = New StreamReader(httpResponse.GetResponseStream())
+                    Dim st = streamReader.ReadToEnd()
+                    answer = JsonConvert.DeserializeObject(Of Dictionary(Of String, ISOImage))(If(st = "[]", "{}", st))
+                End Using
+            End If
+            Return New ISOImageResult With {.ApiResponse = httpResponse, .ISOImages = answer}
+        End Function
+
+        ''' <summary>
+        ''' Create a new ISO image on the current account. The ISO image will be downloaded from a given URL. Download status can be checked with the GetISOImages call.
+        ''' </summary>
+        ''' <param name="URL">Remote URL from where the ISO will be downloaded.</param>
+        ''' <returns>Returns backup list and HTTP API Respopnse.</returns>
+        Function CreateISOImage(ByVal URL As Uri) As ISOImageCreateResult
+
+            Dim dict As New List(Of KeyValuePair(Of String, Object))
+            dict.Add(New KeyValuePair(Of String, Object)("url", URL.AbsoluteUri))
+
+            Dim answer As New ISOImageCreateResult
+            Dim httpResponse = Extensions.ApiClient.ApiExecute("iso/create_from_url", _ApiKey, dict, "POST")
+
+            If httpResponse.StatusCode = 200 Then
+                Using streamReader = New StreamReader(httpResponse.GetResponseStream())
+                    Dim st = streamReader.ReadToEnd()
+                    answer.ISOImage = JsonConvert.DeserializeObject(Of ISOImage)(If(st = "[]", "{}", st))
+                End Using
+            End If
+
+            Return New ISOImageCreateResult With {.ApiResponse = httpResponse, .ISOImage = answer.ISOImage}
+
+        End Function
+    End Class
+End Namespace
---
+++ Models/Responses/ISOImage.vb
@@ -0,0 +1,25 @@
+﻿Imports System.ComponentModel
+Imports System.Net
+
+Namespace API.Models.Responses
+    Public Class ISOImage
+        Public Property ISOID As Integer
+        Public Property name As String
+        Public Property description As String
+        Public Property date_created As String
+        Public Property filename As String
+        Public Property size As Integer
+        Public Property md5sum As String
+        Public Property status As String
+    End Class
+
+    Public Structure ISOImageCreateResult
+        Public Property ISOImage As ISOImage
+        Public Property ApiResponse As HttpWebResponse
+    End Structure
+
+    Public Structure ISOImageResult
+        Public Property ISOImages As Dictionary(Of String, ISOImage)
+        Public Property ApiResponse As HttpWebResponse
+    End Structure
+End Namespace
